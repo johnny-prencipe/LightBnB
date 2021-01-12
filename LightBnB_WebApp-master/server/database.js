ccaf10b61fe88b5744416cef01accfce05e1f389
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-01-12 12:08:32 +0000

    refactor addUser();

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -56,10 +56,15 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queries = [user.name, user.email, user.password];
+
+  return pool.query(`
+  INSERT INTO users
+    (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *`, queries)
+
+  .then(res => res.rows[0]);
 }
 exports.addUser = addUser;
 
